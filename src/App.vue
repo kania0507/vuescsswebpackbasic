6463bf7a9439a96eb692400c2440a64e29384997
Anna KC <29369408+kania0507@users.noreply.github.com>
2021-01-30 21:12:06 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">  
+   
+  </div>
+</template> 
+<script>
+
+export default {
+ name: 'app',
+ components: {
+    Jobs
+  },
+  
+  data: function () {
+    return { 
+    }
+  },
+  mounted: function() {   
+  },
+  methods: {  
+  }
+}
+</script>
+
+<style lang="scss">
+@import "/assets/scss/app.scss";
+</style>
